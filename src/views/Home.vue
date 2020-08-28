5c4723808a16c77b186cb97b6753392f3dc60a05
Upasana Pradhan <upapradhan@gmail.com>
2020-08-28 12:21:04 +0545

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is an home page</h1>
+     <HelloWorld/>
   </div>
 </template>
 
